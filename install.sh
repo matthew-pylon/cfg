26fb07013f2d4268782a1b145bfc7a243757fbab
Matthew Farkas-Dyck <matthew@pylonlending.com>
2024-10-03 12:46:22 -0700

    git

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ cp -R ~/.dotfiles/terminfo/* ~/.terminfo/
 cp ~/.dotfiles/emacs.el ~/.emacs.el
 
 cp ~/.dotfiles/bash_aliases ~/.bash_aliases
+
+cat >>~/.gitconfig <<EOF
+[include]
+	path=.dotfiles/git
+EOF
